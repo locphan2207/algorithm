4c9dab1e7a8b40faf361d1e632498d4037fad0f1
locphan2207 <locphan2207@gmail.com>
2018-04-21 13:40:20 -0700

    find path in grid

---
+++ recursive_dynamic/findPath.js
@@ -0,0 +1,42 @@
+// Imagine there is a robot sitting on the upper left corner of grid with r rows and c columns.
+// THe robot can only move in 2 directions, right and down.
+// THere are cells that robot cannot step on them.
+// Find all the paths the robot can go to get to the bottom right of the grid.
+
+// ----Solution:
+// Same idea as the stair hopping problem. We store all the previous result (
+// or result of adjacent cells) then add one more step to them.
+// For this problem, each cell has two previous result, one from the top cell,
+// one from the left cell, we have to add step to both, then combine them together.
+// Start from the destination, we look at the adjecency cell results
+// and add one more step into the results
+
+function findPath(row, col, result = {'00': ['0,0']} ) {
+  if (row === 0 && col === 0) return result[`${row}${col}`];
+  if (result[`${row}${col}`]) return result[`${row}${col}`];
+  // console.log(result);
+  let topPath = [];
+  let leftPath = [];
+  if (row-1 >= 0) {
+    if (!result[`${row-1}${col}`]) {
+      result[`${row-1}${col}`] = findPath(row-1, col, result);
+    }
+    result[`${row-1}${col}`].forEach(path => {
+      topPath.push(path + `->${row},${col}`);
+    });
+  }
+  if (col-1 >= 0) {
+    if (!result[`${row}${col-1}`]) {
+      result[`${row}${col-1}`] = findPath(row, col-1, result);
+    }
+    result[`${row}${col-1}`].forEach(path => {
+      leftPath.push(path + `->${row},${col}`);
+    });
+  }
+  result[`${row}${col}`] = topPath.concat(leftPath);
+  // console.log(result);
+  return result[`${row}${col}`];
+}
+
+console.log(findPath(1,1));
+console.log(findPath(2,2));
